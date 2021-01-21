5af7dfecac5f540f2e933966fd59d54da1a750c1
arielflima <arielflima@gmail.com>
2021-01-21 18:34:04 -0300

    create react-native-cli, eslint, prettier and src folder

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const pages: React.FC = () => {
+  return (
+    <Container>
+      <Text>TESTANDO</Text>
+    </Container>
+  );
+};
+
+export default pages;
